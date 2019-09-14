ef1674bc9d57f15dc7437a6fb9dbc93ca4147bee
anir16293 <aniruddha16293@gmail.com>
2019-09-14 03:03:35 -0400

    Fixed bug

--- marco_search.py
+++ marco_search.py
@@ -13,7 +13,8 @@ class marcoSearch:
 
     def translate_string(self, list_of_strings: List[str] , destination_language: str) -> List[str]:
         translations = translator.translate(list_of_strings, dest = destination_language)
-        translated_strings = [s.text for s in translations]
+        #translated_strings = [s.text for s in translations]
+        translated_strings = translations.text
         return(translated_strings)
 
     def search_single_query(self, query: str) -> dict:
